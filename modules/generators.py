9e00e1dc4b65755794596f1fd1363417b296cfba
vikrosj <31435660+vikrosj@users.noreply.github.com>
2018-12-29 15:31:04 +0100

    data generators for VAe and MDN

---
+++ modules/generators.py
@@ -0,0 +1,124 @@
+import numpy as np
+import keras
+import h5py
+
+class VAEDataGenerator(keras.utils.Sequence):
+    'Generates data for Keras'
+    def __init__(self, 
+                 list_IDs,
+                 enc_dir,
+                 dec_dir,
+                 batch_size=32, 
+                 dim=(16,131), 
+                 shuffle=True):
+        
+        'Initialization'
+        self.enc_dir = enc_dir
+        self.dec_dir = dec_dir
+        self.dim = dim
+        self.batch_size = batch_size
+        self.list_IDs = list_IDs
+        self.shuffle = shuffle
+        self.on_epoch_end()
+
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return int(len(self.list_IDs) // self.batch_size)
+
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        # Generate indexes of the batch
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+
+        # Find list of IDs
+        list_IDs_temp = [self.list_IDs[k] for k in indexes]
+
+        # Generate data
+        X, y = self.__data_generation(list_IDs_temp)
+
+        return X, y
+
+    def on_epoch_end(self):
+        'Updates indexes after each epoch'
+        self.indexes = np.arange(len(self.list_IDs))
+
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+
+    def __data_generation(self, list_IDs_temp):
+        """Generates data containing batch_size samples. Gets encoder and
+        decoder input from separate files, with same ID"""
+
+        # Initialization
+        encoder_inputs = np.empty((self.batch_size, *self.dim))
+        decoder_inputs = np.empty((self.batch_size, *self.dim))
+
+        # Generate data
+        for i, ID in enumerate(list_IDs_temp):
+            # Store enc input
+            encoder_inputs[i,] = np.load(self.enc_dir + '/' + ID + '.npy')
+
+            decoder_inputs[i,] = np.load(self.dec_dir + '/' + ID + '.npy')
+
+        return ([encoder_inputs, decoder_inputs], encoder_inputs)
+
+class MDNDataGenerator(keras.utils.Sequence):
+    'Generates data for Keras'
+
+    def __init__(self, list_IDs,
+                 dataset_path="h5_files/mdn_dataset_b0.2.h5",
+                 batch_size=32,
+                 dim=(16,64),
+                 shuffle=True):
+
+        'Initialization'
+        self.dim = dim
+        self.batch_size = batch_size
+        self.list_IDs = list_IDs
+        self.shuffle = shuffle
+        self.dataset_path = dataset_path
+        self.on_epoch_end()
+
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return int(len(self.list_IDs) // self.batch_size)
+
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        # Generate indexes of the batch
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+
+        # Find list of IDs
+        list_IDs_temp = [self.list_IDs[k] for k in indexes]
+
+        # Generate data
+        X, y = self.__data_generation(list_IDs_temp)
+
+        return X, y
+
+    def on_epoch_end(self):
+        'Updates indexes after each epoch'
+        self.indexes = np.arange(len(self.list_IDs))
+
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+
+    def __data_generation(self, list_IDs_temp):
+        """Generates data containing batch_size samples. Gets encoder and
+        decoder input from separate files, with same ID"""
+
+        with h5py.File(self.dataset_path, "r") as h5pyFile:
+
+            # Initialization
+            mdn_inputs = np.empty((self.batch_size, *self.dim))
+            target = np.empty((self.batch_size, *self.dim))
+
+            # Generate data
+            for i, ID in enumerate(list_IDs_temp):
+
+                # Store enc input
+                mdn_inputs[i,] = np.array(h5pyFile.get('z_x_' + ID )).reshape(*self.dim)
+
+                target[i,] = np.array(h5pyFile.get('z_y_' + ID )).reshape(*self.dim)
+
+        return mdn_inputs, target
